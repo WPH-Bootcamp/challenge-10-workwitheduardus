626fb4c9c92c30a6afd649251869a7ca2e5f9637
workwitheduardus <workwitheduardus@gmail.com>
2026-06-10 19:52:24 +0700

    feat lib/query/resto content

---
+++ src/lib/query/resto.ts
@@ -0,0 +1,15 @@
+import api from "../api/axios";
+import type { Restaurant, GetAllRestaurantsParams } from "@/types";
+
+//  restoApi
+
+export const restoApi = {
+  getRecommended: (): Promise<Restaurant[]> =>
+    api.get<Restaurant[]>("/restaurants/recommended").then((r) => r.data),
+
+  getAll: (params?: GetAllRestaurantsParams): Promise<Restaurant[]> =>
+    api.get<Restaurant[]>("/restaurants", { params }).then((r) => r.data),
+
+  getById: (id: string): Promise<Restaurant> =>
+    api.get<Restaurant>(`/restaurants/${id}`).then((r) => r.data),
+};
